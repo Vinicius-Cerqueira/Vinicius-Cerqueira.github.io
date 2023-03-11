d0a89c66fcd18354b2fa482788f9d532e6b28299
Vinícius Cerqueira <viniciusvcb10@gmail.com>
2023-03-11 12:12:45 -0300

    edit webscraping script

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,21 @@ function App(props) {
   }
   return (
     <>
-      <div className="App">
+      <table>
+        <tr>
+          <th>Question</th>
+          <th>Link</th>
+        </tr>
+
+        {notClicked.map((a) => {
+          return <tr key={a.Address} className='not-clicked'>
+            <td>Topic {a.Topic} Question {a.Question}</td>
+            <td><a href={a.Address} target="_blank" onClick={() => { setClicked([...clicked, a]); removeQuestion(a) }} > {a.Address} </a></td>
+          </tr>
+        })}
+
+      </table>
+      {/* <div className="App">
         {notClicked.map((a) => {
           return <a key={a.Address} className='not-clicked' href={a.Address} target="_blank" onClick={() => { setClicked([...clicked, a]); removeQuestion(a) }}> T{a.Topic} Q{a.Question} </a>
         })}
@@ -20,7 +34,7 @@ function App(props) {
 
         <h1>Questões Resolvidas</h1>
         {clicked.map((a) => { return <a key={a.Address} className='clicked' href={a.Address} target="_blank"> T{a.Topic} Q{a.Question} </a> })}
-      </div>
+      </div> */}
     </>
   )
 }
